4800c40d403d85e01dff1bd2729423e13b119b55
GillesQuodbach <quodbach.gilles@gmail.com>
2026-02-13 22:12:35 +0100

    setup config file

--- src/config/app_config.rs
+++ src/config/app_config.rs
@@ -1,13 +1,14 @@
 use std::path::PathBuf;
 use directories::ProjectDirs;
 use std::fs;
+use serde::{Deserialize, Serialize};
 
-
+#[derive(Debug, Serialize, Deserialize)]
 pub struct AppConfig {
     pub team: String,
 }
 
-// On recupere le chemin du dossier suivant l'OS
+// Ou est le dosssier ?
 pub fn get_config_dir() -> Result<PathBuf, String> {
     let proj_dir = ProjectDirs::from("fr","aps","branchgen-cli")
     .ok_or("Unable to find config dir")?;
@@ -18,12 +19,27 @@ pub fn get_config_dir() -> Result<PathBuf, String> {
     Ok(config_path.to_path_buf())
 }
 
+// quel est le chemin du fichier ?
 pub fn get_config_file_path() -> Result<PathBuf, String> {
     let config_file = get_config_dir()?.join("config.json");
     Ok(config_file)
 }
 
+// est-ce qu'il existe ?
 pub fn config_file_exists() -> Result<bool, String> {
     let config_file = get_config_file_path()?;
     config_file.try_exists().map_err(|e| format!("Check your configuration file: {}", e))
+}
+
+// creation/ecriture du fichier
+pub fn create_config_file(team: String) -> Result<AppConfig, String> {
+    let config_file_path = get_config_file_path()?;
+
+    let config = AppConfig { team };
+
+    let json = serde_json::to_string_pretty(&config).map_err(|e| format!("Unable to serialize config: {}", e))?;
+
+    std::fs::write(config_file_path, json).map_err(|e| format!("Unable to write config file: {}", e))?;
+
+    Ok(config)
 }
--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,35 @@ mod storage;
 mod error;
 mod config;
 
-use crate::config::app_config::{get_config_dir, AppConfig};
-
+use crate::config::app_config::{config_file_exists, create_config_file, get_config_dir, AppConfig};
+use std::io::{ stdin, stdout, Write};
 
 fn main() {
     println!("*********************");
     println!("Welcome to BranchGen");
     println!("*********************");
 
-    let config_dir = get_config_dir().expect("Unable to find config dir");
-    
+    let config_file_exists = match config_file_exists() {
+        Ok(file) => file,
+        Err(e) => {
+            eprintln!("Error while checking config file: {e}");
+            return;
+        }
+    };
+
+    if config_file_exists {
+        println!("Config file already exists");
+        return;
+    }
+        let mut input_team = String::new();
+        println!("It's your first launch");
+        println!("Please enter your team name");
+        stdin().read_line(&mut input_team).unwrap();
+        let team_name = input_team.trim().to_uppercase();
+
+        if let Err(e) = create_config_file(team_name) {
+            eprintln!("Error while creating config file: {e}");
+        }
+
+
 }
