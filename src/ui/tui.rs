1cf9dd499584d00972c736ad2f2820e669d94cd2
GillesQuodbach <quodbach.gilles@gmail.com>
2026-03-08 14:26:46 +0100

    ratatui layout setup

--- src/ui/tui.rs
+++ src/ui/tui.rs
@@ -29,7 +29,7 @@ pub fn render(frame: &mut Frame, state: &AppState) {
     // decoupe colone de droite
     let right_vertical_chunks = Layout::default()
         .direction(Direction::Vertical)
-        .constraints([Constraint::Length(4), Constraint::Fill(1)]).split(horizontal_chunks[1]);
+        .constraints([Constraint::Length(6), Constraint::Fill(1)]).split(horizontal_chunks[1]);
 
     // panneau gauche
     let left_main_block = Block::bordered()
@@ -45,26 +45,59 @@ pub fn render(frame: &mut Frame, state: &AppState) {
         .constraints([
             Constraint::Length(3),
             Constraint::Length(3),
-            Constraint::Fill(1),
+            Constraint::Length(3),
+            Constraint::Length(3),
+            Constraint::Length(3),
+            Constraint::Length(3),
+            Constraint::Length(3),
         ]).split(left_main_block_inner);
 
 
-    Block::bordered()
+    let pi_block = Block::bordered()
         .title(" PI ")
-        .border_style(Style::default().fg(Color::Green))
-        .render(left_chunks[0], frame.buffer_mut());
+        .border_style(Style::default().fg(Color::Green));
 
-    // panneau git
-    Block::bordered()
+    let it_block = Block::bordered()
+        .title(" IT ")
+        .border_style(Style::default().fg(Color::Green));
+
+    let story_type_block = Block::bordered()
+        .title(" Story type ")
+        .border_style(Style::default().fg(Color::Green));
+
+    let commit_type_block = Block::bordered()
+        .title(" Commit type ")
+        .border_style(Style::default().fg(Color::Green));
+
+    let story_number_block = Block::bordered()
+        .title(" Story number ")
+        .border_style(Style::default().fg(Color::Green));
+
+    let story_title = Block::bordered()
+        .title(" Story title ")
+        .border_style(Style::default().fg(Color::Green));
+
+    let commit_message_block = Block::bordered()
+        .title(" Commit message ")
+        .border_style(Style::default().fg(Color::Green));
+
+    let github_block = Block::bordered()
         .title(" GitHub ")
-        .border_style(Style::default().fg(Color::Green))
-        .render(right_vertical_chunks[0], frame.buffer_mut());
+        .border_style(Style::default().fg(Color::Green));
 
-    // panneau history
-    Block::bordered()
+    let history_block = Block::bordered()
         .title(" History ")
-        .border_style(Style::default().fg(Color::Yellow))
-        .render(right_vertical_chunks[1], frame.buffer_mut());
-    
+        .border_style(Style::default().fg(Color::Yellow));
+
+    pi_block.render(left_chunks[0], frame.buffer_mut());
+    it_block.render(left_chunks[1], frame.buffer_mut());
+    story_type_block.render(left_chunks[2], frame.buffer_mut());
+    commit_type_block.render(left_chunks[3], frame.buffer_mut());
+    story_number_block.render(left_chunks[4], frame.buffer_mut());
+    story_title.render(left_chunks[5], frame.buffer_mut());
+    commit_message_block.render(left_chunks[6], frame.buffer_mut());
+    github_block.render(right_vertical_chunks[0], frame.buffer_mut());
+    history_block.render(right_vertical_chunks[1], frame.buffer_mut());
+
 
 }
