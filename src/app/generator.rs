40780fba0ad0d8691aaded2e06d695cee8a10880
GillesQuodbach <quodbach.gilles@gmail.com>
2026-04-03 17:19:57 +0200

    todo brancher historique

--- src/app/generator.rs
+++ src/app/generator.rs
@@ -1,10 +1,10 @@
 use crate::domain::types::{GeneratedOutput, WorkItemInput};
 
 pub fn build_generated_output(input: &WorkItemInput, team_name: &str) -> Result<GeneratedOutput, String> {
-    let branch_name = input.branch_name(&team_name)?;
+    let branch_name = input.branch_name(team_name)?;
     let checkout_cmd = GeneratedOutput::format_checkout_cmd(&branch_name);
-    let commit_msg = input.commit_name(&team_name)?;
-    let pr_title = input.pr_name(&team_name)?;
+    let commit_msg = input.commit_name(team_name)?;
+    let pr_title = input.pr_name(team_name)?;
 
     Ok(GeneratedOutput::new(
         checkout_cmd,
