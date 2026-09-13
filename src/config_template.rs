29a584ce34f0ad092f467aa3f3989b4afe95feef
codeBash <quodbach.gilles@gmail.com>
2026-09-13 15:43:11 +0200

    fix config template not usable

--- src/config_template.rs
+++ src/config_template.rs
@@ -2,27 +2,27 @@ pub const DEFAULT_CONFIG: &str = r#"
 # Types de champs disponibles : text, number, select
 # Les clés définies ici doivent correspondre aux variables dans tes formats : {ma_cle}
 
-# [[fields]]
-# key      = "ticket"
-# label    = "Numéro de ticket"
-# type     = "number"
-# required = true
+[[fields]]
+key      = "ticket"
+label    = "Numéro de ticket"
+type     = "number"
+required = true
 
-# [[fields]]
-# key      = "description"
-# label    = "Description"
-# type     = "text"
-# required = true
+[[fields]]
+key      = "description"
+label    = "Description"
+type     = "text"
+required = true
 
-# [[fields]]
-# key    = "type"
-# label  = "Type"
-# type   = "select"
-# values = ["feat", "fix", "refactor", "docs", "chore"]
-# required = true
+[[fields]]
+key    = "type"
+label  = "Type"
+type   = "select"
+values = ["feat", "fix", "refactor", "docs", "chore"]
+required = true
 
-# [formats]
-# branch   = "{type}/{ticket}-{description}"
-# commit   = "{type}: {description}"
-# pr_title = "[{ticket}] {description}"
+[formats]
+branch   = "{type}/{ticket}-{description}"
+commit   = "{type}: {description}"
+pr_title = "[{ticket}] {description}"
 "#;
