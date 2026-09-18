d827dc23bcef3f1b047c3f2d3ff4c983b8cd04d7
Gilles Quodbach <83692636+GillesQuodbach@users.noreply.github.com>
2026-09-18 14:01:24 +0200

    feat: configure default template for ASPICOT team

--- src/config_template.rs
+++ src/config_template.rs
@@ -1,28 +1,66 @@
 pub const DEFAULT_CONFIG: &str = r#"
-# Types de champs disponibles : text, number, select
-# Les clés définies ici doivent correspondre aux variables dans tes formats : {ma_cle}
+# Configuration par défaut de l'équipe ASPICOT
+# Le fichier est directement utilisable après l'initialisation.
+# Types disponibles : text, number, select
+# Normalisations : none, spaces, uppercase, lowercase,
+#                  lowercasefirstletter, uppercasefirstletter
 
 [[fields]]
-key      = "ticket"
-label    = "Numéro de ticket"
+key        = "git"
+label      = "Git"
+type       = "select"
+values     = ["ASPICOT"]
+required   = true
+persistent = true
+
+[[fields]]
+key      = "pi"
+label    = "PI"
 type     = "number"
 required = true
 
 [[fields]]
-key      = "description"
-label    = "Description"
+key      = "it"
+label    = "IT"
 type     = "text"
 required = true
 
 [[fields]]
-key    = "type"
-label  = "Type"
-type   = "select"
-values = ["feat", "fix", "refactor", "docs", "chore"]
+key      = "story_type"
+label    = "Type Story"
+type     = "select"
+values   = ["feature", "bug", "task"]
 required = true
 
+[[fields]]
+key      = "commit_type"
+label    = "Type commit"
+type     = "select"
+values   = ["feat", "fix", "refactor", "docs", "chore"]
+required = true
+
+[[fields]]
+key        = "story_ref"
+label      = "Story/Defect Ref (S-X...)"
+type       = "text"
+required   = true
+normalize  = "lowercase"
+
+[[fields]]
+key      = "task_ref"
+label    = "Task Ref (tk-xxxxxx)"
+type     = "text"
+required = false
+
+[[fields]]
+key       = "story_title"
+label     = "Story title"
+type      = "text"
+required  = true
+normalize = "spaces"
+
 [formats]
-branch   = "{type}/{ticket}-{description}"
-commit   = "{type}: {description}"
-pr_title = "[{ticket}] {description}"
+branch   = "{story_type}/{pi}-{it}_{git}_{story_ref}_{story_title}"
+commit   = "AS [{pi}-{it}] #{story_ref} - {commit_type}: details to update"
+pr_title = "{commit_type}: {pi}-{it}_{git}_{story_ref}_{story_title}"
 "#;
